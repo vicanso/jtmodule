4661454d7e30c26a67ae893f7a1ad43b324857c9
vicanso <vicansocanbico@gmail.com>
2014-01-09 19:51:13 +0800

    commit

--- index.coffee
+++ index.coffee
@@ -6,9 +6,22 @@ fs = require 'fs'
 
 module.exports.defineModule = (file) ->
   "module = GLOBAL_MODULES['#{file}'] = {id : '#{file}'}\nexports = module.exports = {}\n"
+FILTER_FILES = null
+module.exports.setFilterFiles = (files) ->
+  FILTER_FILES = files
+
+
+CONTEXT = null
+module.exports.setContext = (ctx) ->
+  CONTEXT = ctx
 
 getDependencies = module.exports.getDependencies = (staticPath, file, hasCheckedFiles = []) ->
   result = []
+  exceptToGetDep = false
+  if FILTER_FILES?.length
+    exceptToGetDep = true if ~_.indexOf FILTER_FILES, file
+  file = path.join '/components', file if file[0] != '.' && file[0] != '/'
+  file += '.js' if !path.extname file
   if ~_.indexOf hasCheckedFiles, file
     return result
   else
@@ -17,6 +30,7 @@ getDependencies = module.exports.getDependencies = (staticPath, file, hasChecked
     readFile = path.join staticPath, file 
   else
     readFile = file
+  console.dir file
   requireFiles = []
   if !fs.existsSync readFile
     readFile = readFile.replace path.extname(readFile), '.coffee'
@@ -28,21 +42,24 @@ getDependencies = module.exports.getDependencies = (staticPath, file, hasChecked
       return
   else
     code = fs.readFileSync readFile, 'utf8'
-  ctx = 
-    GLOBAL_MODULES : {}
-    module :
+  if !exceptToGetDep
+    ctx = 
+      GLOBAL_MODULES : {}
+      module :
+        exports : {}
       exports : {}
-    exports : {}
-    require : (subFile) ->
-      subFile += '.js' if !path.extname subFile
-      requireFiles.push subFile
-  try
-    vm.runInNewContext code, ctx
-  catch e
-    console.error e
-  _.each requireFiles, (tmpFile) =>
-    tmpFile = path.join path.dirname(file), tmpFile if tmpFile[0] == '.'
-    if !~_.indexOf result, tmpFile
-      result.push.apply result, getDependencies staticPath, tmpFile, hasCheckedFiles
+      require : (subFile) ->
+        # subFile += '.js' if !path.extname subFile
+        requireFiles.push subFile
+    _.extend ctx, CONTEXT
+    try
+      vm.runInNewContext code, ctx
+    catch e
+      console.error file
+      console.error e
+    _.each requireFiles, (tmpFile) =>
+      tmpFile = path.join path.dirname(file), tmpFile if tmpFile[0] == '.'
+      if !~_.indexOf result, tmpFile
+        result.push.apply result, getDependencies staticPath, tmpFile, hasCheckedFiles
   result.push file
   _.uniq _.flatten result
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 (function() {
-  var coffeeScript, fs, getDependencies, path, vm, _;
+  var CONTEXT, FILTER_FILES, coffeeScript, fs, getDependencies, path, vm, _;
 
   _ = require('underscore');
 
@@ -15,13 +15,37 @@
     return "module = GLOBAL_MODULES['" + file + "'] = {id : '" + file + "'}\nexports = module.exports = {}\n";
   };
 
+  FILTER_FILES = null;
+
+  module.exports.setFilterFiles = function(files) {
+    return FILTER_FILES = files;
+  };
+
+  CONTEXT = null;
+
+  module.exports.setContext = function(ctx) {
+    return CONTEXT = ctx;
+  };
+
   getDependencies = module.exports.getDependencies = function(staticPath, file, hasCheckedFiles) {
-    var code, ctx, readFile, requireFiles, result,
+    var code, ctx, exceptToGetDep, readFile, requireFiles, result,
       _this = this;
     if (hasCheckedFiles == null) {
       hasCheckedFiles = [];
     }
     result = [];
+    exceptToGetDep = false;
+    if (FILTER_FILES != null ? FILTER_FILES.length : void 0) {
+      if (~_.indexOf(FILTER_FILES, file)) {
+        exceptToGetDep = true;
+      }
+    }
+    if (file[0] !== '.' && file[0] !== '/') {
+      file = path.join('/components', file);
+    }
+    if (!path.extname(file)) {
+      file += '.js';
+    }
     if (~_.indexOf(hasCheckedFiles, file)) {
       return result;
     } else {
@@ -32,6 +56,7 @@
     } else {
       readFile = file;
     }
+    console.dir(file);
     requireFiles = [];
     if (!fs.existsSync(readFile)) {
       readFile = readFile.replace(path.extname(readFile), '.coffee');
@@ -47,32 +72,33 @@
     } else {
       code = fs.readFileSync(readFile, 'utf8');
     }
-    ctx = {
-      GLOBAL_MODULES: {},
-      module: {
-        exports: {}
-      },
-      exports: {},
-      require: function(subFile) {
-        if (!path.extname(subFile)) {
-          subFile += '.js';
+    if (!exceptToGetDep) {
+      ctx = {
+        GLOBAL_MODULES: {},
+        module: {
+          exports: {}
+        },
+        exports: {},
+        require: function(subFile) {
+          return requireFiles.push(subFile);
         }
-        return requireFiles.push(subFile);
+      };
+      _.extend(ctx, CONTEXT);
+      try {
+        vm.runInNewContext(code, ctx);
+      } catch (e) {
+        console.error(file);
+        console.error(e);
       }
-    };
-    try {
-      vm.runInNewContext(code, ctx);
-    } catch (e) {
-      console.error(e);
+      _.each(requireFiles, function(tmpFile) {
+        if (tmpFile[0] === '.') {
+          tmpFile = path.join(path.dirname(file), tmpFile);
+        }
+        if (!~_.indexOf(result, tmpFile)) {
+          return result.push.apply(result, getDependencies(staticPath, tmpFile, hasCheckedFiles));
+        }
+      });
     }
-    _.each(requireFiles, function(tmpFile) {
-      if (tmpFile[0] === '.') {
-        tmpFile = path.join(path.dirname(file), tmpFile);
-      }
-      if (!~_.indexOf(result, tmpFile)) {
-        return result.push.apply(result, getDependencies(staticPath, tmpFile, hasCheckedFiles));
-      }
-    });
     result.push(file);
     return _.uniq(_.flatten(result));
   };
